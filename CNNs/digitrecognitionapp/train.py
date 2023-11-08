99085d2cde3c332c94985453a067d8944d9a649a
omar-ntu <105975781+omar-ntu@users.noreply.github.com>
2023-11-09 01:01:35 +0800

    Update train.py

--- CNNs/digitrecognitionapp/train.py
+++ CNNs/digitrecognitionapp/train.py
@@ -1,106 +1,73 @@
-import torch
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader
-from torch import optim
-from torch.autograd import Variable
-import torch.nn as nn
 import numpy as np
+import torch
+import torchvision
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+from torch.utils.data import DataLoader
 
-from model import CNN
+from model import Model
 
-SAVE_MODEL_PATH = "checkpoint/model.pth"
+SAVE_MODEL_PATH = "best_accuracy.pth"
 
-train_data = datasets.MNIST(
-    root = 'data',
-    train = True,
-    transform = ToTensor(),
-    download = True,
-)
+def train():
+    transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
+    train = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+    train_loader = DataLoader(train, batch_size=64)
+    test = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
+    test_loader = DataLoader(test, batch_size=64)
 
-test_data = datasets.MNIST(
-    root = 'data',
-    train = False,
-    transform = ToTensor()
-)
-
-loaders = {
-    'train': torch.utils.data.DataLoader(train_data,
-                                        batch_size=100,
-                                        shuffle=True,
-                                        num_workers=1),
-    'test': torch.utils.data.DataLoader(test_data,
-                                       batch_size=100,
-                                       shuffle=True,
-                                       num_workers=1),
-}
-
-# Loss function and Optimizer
-loss_func = nn.CrossEntropyLoss()
-cnn = CNN()
-optimizer = optim.Adam(cnn.parameters(), lr = 0.01)
-
-# Number of Epochs
-num_epochs = 10
-
-def train(num_epochs, cnn, loaders):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     print("device: ", device)
+    model = Model().to(device)
 
-    cnn = cnn.to(device)
-    
-    cnn.train()
-        
-    # Train the model
-    total_step = len(loaders['train'])
-        
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+    # Training
+    num_epochs = 30
+    best_eval_acc = 0
+    validation_interval = 200
+    start = time()
+
     for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(loaders['train']):
+        avg_loss = 0
+        for i, (imgs, labels) in enumerate(train_loader):
+            imgs, labels = imgs.to(device), labels.to(device)
+            optimizer.zero_grad() # zero the parameter gradients
+            preds = model(imgs) # forward pass
+            loss = criterion(preds, labels) # calculate loss
+            loss.backward() # backpropagation
+            optimizer.step() # optimization
             
-            # gives batch data, normalize x when iterate train_loader
-            b_x = Variable(images)   # batch x
-            b_y = Variable(labels)   # batch y
-    
-            output = cnn(b_x)[0]               
-            loss = loss_func(output, b_y)
-            
-            # clear gradients for this training step   
-            optimizer.zero_grad()           
-            
-            # backpropagation, compute gradients 
-            loss.backward()    
-            # apply gradients             
-            optimizer.step()                
-            
+            # print statistics
+            avg_loss += loss.item()
             if (i+1) % 100 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
-                pass
-        
-            pass
-    
-    
-        pass
-    torch.save(cnn.state_dict(), SAVE_MODEL_PATH)
+                print(f"Epoch [{epoch + 1}/{num_epochs}], Batch [{i+1}], Loss: {avg_loss / 100:.3f}")
+                avg_loss = 0
+            
+            # validation
+            if (i+1) % validation_interval == 0: # perform validation every 200 batch
+                model.eval()
+                correct, total = 0, 0
+                for i, (imgs, labels) in enumerate(test_loader):
+                    imgs, labels = imgs.to(device), labels.to(device)
+                    with torch.no_grad():
+                        preds = model(imgs)
+                    preds = torch.argmax(preds, dim=1)
+                    correct += (preds == labels).sum().item()
+                    total += labels.shape[0]
+                accuracy = correct / total
+                print(f"eval acc: {accuracy:.4f}")
 
-
-
-def test():
-    # Test the model
-    cnn.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in loaders['test']:
-            test_output, last_layer = cnn(images)
-            pred_y = torch.max(test_output, 1)[1].data.squeeze()
-            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
-            pass
-    print('Test Accuracy of the model on the 10000 test images: %.2f' % accuracy)
-    
-    pass
-
+                # save model
+                if accuracy > best_eval_acc:
+                    best_eval_acc = accuracy
+                    torch.save(model.state_dict(), SAVE_MODEL_PATH)
+        print(f"{epoch + 1}/{num_epochs} finished. Elapsed time: {time() - start:.1f} secs")
+    print(f"Training finished. Best evaluation accuracy: {best_eval_acc:.4f}")
 
 if __name__ == "__main__":
-    train(num_epochs, cnn, loaders)
-    test()
+    train()
