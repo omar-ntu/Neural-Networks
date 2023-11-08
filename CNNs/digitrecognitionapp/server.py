bca9b22cd24a2b77426f946cc17e41f06764984e
omar-ntu <105975781+omar-ntu@users.noreply.github.com>
2023-11-09 01:02:00 +0800

    Update server.py

--- CNNs/digitrecognitionapp/server.py
+++ CNNs/digitrecognitionapp/server.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 from PIL import Image, ImageChops, ImageOps
 from torchvision import transforms
 
-from model import CNN
+from model import Model
 from train import SAVE_MODEL_PATH
 
 app = Flask(__name__)
@@ -19,7 +19,7 @@ def index():
 @app.route("/DigitRecognition", methods=["POST"])
 def predict_digit():
     img = Image.open(request.files["img"]).convert("L")
-
+    # predict
     res_json = {"pred": "Err", "probs": []}
     if predict is not None:
         res = predict(img)
@@ -30,8 +30,8 @@ def predict_digit():
 
 class Predict():
     def __init__(self):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = CNN().to(device)
+        device = torch.device("cpu")
+        self.model = Model().to(device)
         self.model.load_state_dict(torch.load(SAVE_MODEL_PATH, map_location=device))
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
@@ -41,7 +41,7 @@ class Predict():
         shift_x = (left + (right - left) // 2) - w // 2
         shift_y = (top + (bottom - top) // 2) - h // 2
         return ImageChops.offset(img, -shift_x, -shift_y)
-    
+
     def __call__(self, img):
         img = ImageOps.invert(img)
         img = self._centering_img(img)
@@ -53,9 +53,8 @@ class Predict():
         with torch.no_grad():
             preds = self.model(tensor)
             preds = preds.detach().numpy()[0]
-
         return preds
-    
+
 if __name__ == "__main__":
     import os
     assert os.path.exists(SAVE_MODEL_PATH), "no saved model"
