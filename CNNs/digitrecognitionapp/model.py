618a24ae57130a93f72a375c89a36335782cc392
omar-ntu <105975781+omar-ntu@users.noreply.github.com>
2023-11-09 01:01:15 +0800

    Update model.py

--- CNNs/digitrecognitionapp/model.py
+++ CNNs/digitrecognitionapp/model.py
@@ -1,34 +1,49 @@
 import torch
 import torch.nn as nn
 
-class CNN(nn.Module):
+class Model(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
-        
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=1,
-                out_channels=16,
-                kernel_size=5,
-                stride=1,
-                padding=2,
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+        super(Model, self).__init__()
+
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(
+            kernel_size=2,
+            stride=2
         )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 32, 5, 1, 2),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=32,
+            kernel_size=5,
+            stride=1,
+            padding=2
         )
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, 5, 1, 2)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.fc1 = nn.Linear(64 * 7 * 7, 1024)
+        self.fc2 = nn.Linear(1024, 10)
         
-        #output 10 classes
-        self.out = nn.Linear(32 * 7 * 7, 10)
-        
+        self.softmax = nn.Softmax(dim=1)
+
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
         x = self.conv2(x)
-        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
-        x = x.view(x.size(0), -1)       
-        output = self.out(x)
-        return output    # return x for visualization
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        x = self.fc2(x)
+        x = self.softmax(x)
+
+        return x
